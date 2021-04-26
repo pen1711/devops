b175053b586154804330ea89a49e85badbdd3f45
pen1711 <tubchair1711@gmail.com>
2021-04-26 16:59:12 +0000

    java code

---
+++ bank.java
@@ -0,0 +1,11 @@
+class bank
+{
+        public static void main(){
+                s.o.p("xyz bank");
+                s.o.p("xyz bank");
+                s.o.p("xyz bank");
+                s.o.p("xyz bank");
+
+        }
+}
+
